a291c0f8d6b58bf4364d2b3076ec42d744ae3210
Hebert Costa Andrade <handrade@imcba.com>
2024-06-01 08:03:56 -0300

    4 - Change title to required field

--- src/Domain/Entities/TodoItem.cs
+++ src/Domain/Entities/TodoItem.cs
@@ -4,7 +4,7 @@ public class TodoItem : BaseAuditableEntity
 {
     public int ListId { get; set; }
 
-    public string? Title { get; set; }
+    public required string Title { get; set; }
 
     public string? Note { get; set; }
 
--- src/Domain/Entities/TodoList.cs
+++ src/Domain/Entities/TodoList.cs
@@ -2,7 +2,7 @@
 
 public class TodoList : BaseAuditableEntity
 {
-    public string? Title { get; set; }
+    public required string Title { get; set; }
 
     public Colour Colour { get; set; } = Colour.White;
 
