34e5b398d47e96eb9cb71405de8fd89af9091d8d
Hebert Costa Andrade <handrade@imcba.com>
2024-06-03 18:33:13 -0300

    22 - Add test example

---
+++ test/Application.UnitTests/TodoLists/CreateTodoListCommandHandlerTest.cs
@@ -0,0 +1,36 @@
+﻿using Assignment.Application.Common.Interfaces;
+using Assignment.Application.TodoLists.Commands.CreateTodoList;
+using Assignment.Domain.Entities.Todo;
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Moq;
+
+namespace Application.UnitTests.TodoLists;
+public class CreateTodoListCommandHandlerTest
+{
+    [Fact]
+    public async Task ShouldCreateTodoListSuccesfully()
+    {
+        //Arrange
+        var fixture = new Fixture();
+
+        var command = fixture.Create<CreateTodoListCommand>();
+
+        var context = new Mock<IApplicationDbContext>();
+
+        context.Setup(x => x.TodoLists.AddAsync(It.IsAny<TodoList>(), It.IsAny<CancellationToken>()))
+               .Verifiable();
+        
+        context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()))
+               .Verifiable();
+
+        var handler = new CreateTodoListCommandHandler(context.Object);
+
+        //Act
+        var result = await handler.Handle(command,new CancellationToken());
+
+        //Assert
+        context.Verify();
+    }
+}
